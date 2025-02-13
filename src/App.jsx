919bc83c4b5a406e7980e83fbcfd746e5cf5bd2b
Eliza Atanbaeva <bknade794@gmail.com>
2025-02-13 14:27:57 +0600

    Добавила useState чтобы использовать configProvider для переключении темы, долго думала как использовать useState для перехода туда <-> сюда

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,119 @@
-import { Card, Col, Row, Typography, Button } from "antd";
+import { Card, Col, Row, Typography, Button, ConfigProvider } from 'antd';
+import { useState } from 'react';
 
 const { Title, Text } = Typography;
 
 const tasks = [
   {
     id: 1,
-    title: "Выучить React",
+    title: "Написать код",
     date: "10.01.2025",
     complited: false,
   },
   {
     id: 2,
-    title: "Разобраться с Redux",
+    title: "Прочитать 5 книг",
     date: "20.02.2025",
     complited: true,
   },
   {
     id: 3,
-    title: "Написать проект",
-    date: "30.03.2025",
+    title: "Стать успешным frontend разработчиком апрар попрор",
+    date: "30.05.2025",
     complited: false,
   },
 ];
 
-const App = () => (
-  <Row gutter={[16, 16]} style={{ padding: "20px" }}>
-    {tasks.map((task) => (
-      <Col xs={24} sm={12} md={8} key={task.id}>
-        <Card
-          title={<Title level={4}>{task.title}</Title>}
-          style={{
-            borderRadius: "8px",
-            boxShadow: "0 4px 8px rgba(0,0,0,0.1)",
-            backgroundColor: task.complited ? "#f0f0f0" : "white",
-          }}
+const App = () => {
+  const [isDark, setIsDark] = useState(false);
+
+  const darkTheme = {
+    token: {
+      colorPrimary: '#8B5DFF',
+      colorBgBase: '#8B5DFF',
+      colorTextBase: '#F7F7F7',
+    },
+  };
+
+  const lightTheme = {
+    token: {
+      colorPrimary: '#1890ff',
+      colorBgBase: '#ffffff',
+      colorTextBase: '#000000',
+    },
+  };
+
+ 
+  const toggleTheme = () => {
+    setIsDark(!isDark);
+  };
+
+  return (
+   
+    <ConfigProvider theme={isDark ? darkTheme : lightTheme}>
+      <div style={{ 
+        padding: '20px',
+          backgroundColor: isDark ? '#1f1f1f' : '#ffffff', 
+          minHeight: '100vh'
+           }}>
+        <Button
+          type="default"
+          style={{ marginBottom: '20px' }}
+          onClick={toggleTheme}
         >
-          <Text type="secondary">📅 Дата: {task.date}</Text>
-          <br />
-          <Text strong style={{ color: task.complited ? "gray" : "black" }}>
-            {task.complited ? "✅ Завершено" : "⏳ В процессе"}
-          </Text>
-          <br />
-          <br />
-          {!task.complited && (
-            <Button type="primary" onClick={() => alert("Задача выполнена!")}>
-              Done
-            </Button>
-          )}
-        </Card>
-      </Col>
-    ))}
-  </Row>
-);
+          {isDark ? 'Светлая тема' : 'Темная тема'}
+        </Button>
+
+        <Row gutter={[16, 16]}>
+          {tasks.map((task) => (
+            <Col xs={24} sm={12} md={8} key={task.id}>
+              <Card
+                variant="borderless"
+                title={<Title level={4}
+                style={{
+                  wordWrap: 'break-word',
+                  overflowWrap: 'break-word',
+                  whiteSpace: 'normal',
+                  maxWidth: '100%',
+                }}>{task.title}</Title>}
+                style={{
+                  wordWrap: 'break-word',
+                  wordBreak: 'break-word',
+                  boxShadow: isDark ?'1px 2px 4px rgba(167, 67, 203, 0.9)' : '1px 2px 4px rgba(0, 0, 0, 0.1)',
+                  transform: task.complited ? "scale(0.9)" : "scale(1)", 
+                  transition: "transform 0.3s ease",
+                  backgroundColor: task.complited
+                    ? (isDark ? '#333' : '#f0f0f0') 
+                    : (isDark ? '#222' : 'white')
+                }}
+              >
+                <Text 
+                type="secondary"
+                style={{ color: task.complited
+                    ? (isDark ? '#F7F7F7' : '#000000') 
+                    : (isDark ? '#F7F7F7' : '#000000') }}>📅 Дата: {task.date}</Text>
+                <br />
+                <Text 
+                 strong
+                 style={{ color: task.complited
+                    ? (isDark ? '#F7F7F7' : '#000000') 
+                    : (isDark ? '#F7F7F7' : '#000000'), }}>
+                  {task.complited ? '✅ Завершено' : '⏳ В процессе'}
+                </Text>
+                <br />
+                <br />
+                {!task.complited && (
+                  <Button type="primary" onClick={() => alert('Задача выполнена!')}>
+                    Done
+                  </Button>
+                )}
+              </Card>
+            </Col>
+          ))}
+        </Row>
+      </div>
+    </ConfigProvider>
+  );
+};
 
 export default App;
